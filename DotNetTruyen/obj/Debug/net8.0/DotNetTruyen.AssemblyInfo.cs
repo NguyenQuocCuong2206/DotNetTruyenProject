3f2a68c97c9aa65e0f72a669288ea23d84571193
duongdm03 <minhduongdo0903@gmail.com>
2025-02-20 14:50:28 +0700

    .

--- DotNetTruyen/obj/Debug/net8.0/DotNetTruyen.AssemblyInfo.cs
+++ DotNetTruyen/obj/Debug/net8.0/DotNetTruyen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetTruyen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aada16b97561681ff809d7bbb68f356b4dcb8220")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50bf317815028450420db110ebb7257ffeac7bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetTruyen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetTruyen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
