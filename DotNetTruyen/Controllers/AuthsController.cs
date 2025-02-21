e4b9f57f24072a10e0bea49ba7e7032a5cac7560
nguyenquoccuong <quoccuong2262k0@gmail.com>
2025-02-21 20:22:04 +0700

    Complete Register

--- DotNetTruyen/Controllers/AuthsController.cs
+++ DotNetTruyen/Controllers/AuthsController.cs
@@ -6,14 +6,19 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 using System.Diagnostics;
 using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Text;
+using static System.Net.WebRequestMethods;
 
 namespace DotNetTruyen.Controllers
 {
-    [Authorize]
     [Route("/Auths/[action]")]
     public class AuthsController : Controller
     {
@@ -33,7 +38,6 @@ namespace DotNetTruyen.Controllers
         }
 
         [HttpGet("/login/")]
-        [AllowAnonymous]
         public IActionResult Login(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
@@ -41,7 +45,6 @@ namespace DotNetTruyen.Controllers
         }
 
         [HttpPost("/login/")]
-        [AllowAnonymous]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
         {
@@ -70,16 +73,15 @@ namespace DotNetTruyen.Controllers
                 }
                 else
                 {
-                    ViewData["ErrorMessage"] = "Tên đăng nhập hoặc mật khẩu không chính xác";
+                    ViewBag.ErrorMessage = "Tên đăng nhập hoặc mật khẩu không chính xác";
                     return View();
                 }
             }
-            ViewData["ErrorMessage"] = "Đã xảy ra lỗi khi đăng nhập";
+            ViewBag.ErrorMessage = "Đã xảy ra lỗi khi đăng nhập";
             return View();
         }
 
         [HttpGet("/loginWithGoogle/")]
-        [AllowAnonymous]
         public async Task LoginWithGoogle(string returnUrl = null)
         {
             await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme,
@@ -90,7 +92,6 @@ namespace DotNetTruyen.Controllers
                 });
         }
 
-        [AllowAnonymous]
         public async Task<IActionResult> GoogleResponse(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
@@ -98,8 +99,8 @@ namespace DotNetTruyen.Controllers
             var result = await HttpContext.AuthenticateAsync(GoogleDefaults.AuthenticationScheme);
             if (!result.Succeeded)
             {
-                ViewData["ErrorMessage"] = "Đăng nhập bằng Google không thành công";
-                return LocalRedirect("/login");
+                ViewBag.ErrorMessage = "Đăng nhập bằng Google không thành công";
+                return View("Login");
             }
 
             var claims = result.Principal.Identities.FirstOrDefault().Claims.Select(claim => new
@@ -120,16 +121,16 @@ namespace DotNetTruyen.Controllers
                 var creatResult = await _userManager.CreateAsync(newUser);
                 if (!creatResult.Succeeded)
                 {
-                    ViewData["ErrorMessage"] = "Đăng nhập bằng Google không thành công";
-                    return LocalRedirect("/login");
+                    ViewBag.ErrorMessage = "Đăng nhập bằng Google không thành công";
+                    return View("Login");
                 }
 
-                //var addRoleResult = await _userManager.AddToRoleAsync(newUser, "Reader");
-                //if (!addRoleResult.Succeeded)
-                //{
-                //    ViewData["ErrorMessage"] = "Có lỗi khi cấp quyền cho tài khoản này";
-                //    return LocalRedirect("/login");
-                //}
+                var addRoleResult = await _userManager.AddToRoleAsync(newUser, "Reader");
+                if (!addRoleResult.Succeeded)
+                {
+                    ViewBag.ErrorMessage = "Có lỗi khi cấp quyền cho tài khoản này";
+                    return View("Login");
+                }
 
                 if (creatResult.Succeeded)
                 {
@@ -144,26 +145,104 @@ namespace DotNetTruyen.Controllers
                 await _signInManager.SignInAsync(existingUser, isPersistent: false);
                 return LocalRedirect(returnUrl);
             }
-            return RedirectToAction("/login");
+            return LocalRedirect("/login");
         }
 
-        [HttpGet("/register/")]
-        [AllowAnonymous]
-        public IActionResult Register(string returnUrl = null)
+        [HttpGet("/register")]
+        public IActionResult Register()
         {
-            ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
 
+        [HttpPost("/register")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.UserName, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    var addRoleResult = await _userManager.AddToRoleAsync(user, "Reader");
+                    if (!addRoleResult.Succeeded)
+                    {
+                        ViewBag.ErrorRegisterMessage = "Có lỗi khi cấp quyền cho tài khoản này";
+                        return View();
+                    }
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+                    var callbackUrl = Url.ActionLink(
+                        action: nameof(ConfirmEmail),
+                        values:
+                            new
+                            {
+                                userId = user.Id,
+                                code = code
+                            },
+                        protocol: Request.Scheme);
+
+                    await _emailService.SendEmailAsync(model.Email,
+                        "Xác nhận địa chỉ email",
+                        @$"Bạn đã đăng ký tài khoản trên DotNetTruyen, 
+                           hãy <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>bấm vào đây</a> 
+                           để kích hoạt tài khoản.");
+                    ViewBag.SuccessRegisterMessage = "Hãy truy cập vào email để xác thực việc đăng ký tài khoản";
+                }
+                else
+                {
+                    ViewBag.ErrorRegisterMessage = result.Errors.FirstOrDefault().Description;
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            
+            if (userId == null || code == null)
+            {
+                ViewBag.ErrorRegisterMessage = "Đăng ký không thành công do lỗi xác thực email";
+                return View("Register");
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                ViewBag.ErrorRegisterMessage = "Đăng ký không thành công do lỗi xác thực email";
+                return View("Register");
+            }
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            if (result.Succeeded)
+            {
+                ViewBag.SuccessRegisterMessage = "Đăng ký thành công, hãy đăng nhập ngay";
+                return View("Login");
+            }
+
+            ViewBag.ErrorRegisterMessage = "Đăng ký không thành công do lỗi xác thực email";
+            return View("Register");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Confirm(string userId, string code)
+        {
+
+            ViewBag.ErrorRegisterMessage = "Đăng ký không thành công do lỗi xác thực email";
+            return View("Register");
+        }
+
         [HttpGet("/forgotPassword/")]
-        [AllowAnonymous]
         public IActionResult ForgotPassword()
         {
             return View();
         }
 
         [HttpPost("/forgotPassword/")]
-        [AllowAnonymous]
         public async Task<IActionResult> ForgotPassword(string emailReset)
         {
             await _emailService.SendEmailAsync(emailReset, "Xin chao", "DotNetTruyen đây");
@@ -171,6 +250,7 @@ namespace DotNetTruyen.Controllers
         }
 
         [HttpGet("/logout")]
+        [Authorize]
         public async Task<IActionResult> LogOut()
         {
             await HttpContext.SignOutAsync();
