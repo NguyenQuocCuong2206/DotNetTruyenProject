3a37565c9c6ca4dc4d36f11ea7314e50b2dbea6b
nguyenquoccuong <quoccuong2262k0@gmail.com>
2025-03-04 21:31:14 +0700

    User Profile

--- DotNetTruyen/Program.cs
+++ DotNetTruyen/Program.cs
@@ -40,11 +40,7 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.AccessDeniedPath = "/khongduoctruycap.html";
 });
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-}).AddCookie()
+builder.Services.AddAuthentication()
 .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
 {
     options.ClientId = builder.Configuration["Authentication_Google:ClientId"];
