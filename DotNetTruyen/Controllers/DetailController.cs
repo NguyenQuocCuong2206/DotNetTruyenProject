e1c00c18b1755091745770c76f0f8365b02ed613
Haiyencute8903 <163019470+Haiyencute8903@users.noreply.github.com>
2025-03-19 14:30:02 +0700

    details

---
+++ DotNetTruyen/Controllers/DetailController.cs
@@ -0,0 +1,40 @@
+﻿using DotNetTruyen.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTruyen.Controllers
+{
+    public class DetailController : Controller
+    {
+        private readonly DotNetTruyenDbContext _context;
+
+        public DetailController(DotNetTruyenDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(Guid id)
+        {
+            var comic = _context.Comics
+                .Include(c => c.Chapters)
+                .Include(c => c.ComicGenres)
+                    .ThenInclude(cg => cg.Genre)
+                .Include(c => c.Likes)
+                .Include(c => c.Follows)
+                .FirstOrDefault(c => c.Id == id);
+
+            if (comic == null)
+                return NotFound();
+
+            var comments = _context.Comments
+                .Include(c => c.User)
+                .Where(c => c.ComicId == id)
+                .OrderByDescending(c => c.Date)
+                .ToList();
+
+            ViewBag.Comments = comments;
+
+            return View(comic);
+        }
+    }
+}
